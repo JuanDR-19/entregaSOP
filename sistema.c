0ff3dfb194a6ff765a05d6dc672636294e6f7ad2
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2022-05-25 18:31:33 -0500

    cambio en la linea 268

--- sistema.c
+++ sistema.c
@@ -265,7 +265,7 @@ int main (int argc, char **argv)
           } else creado = 1; 
        }  while (creado == 0);
        suscr[i].pid = datos.pid;
-       suscr[i].topico = datos.topico;    
+       suscr[i].topico = datos.topico[i];    
        BuscarUltima(i,suscr[i].topico);
     }   // Fin del ciclo infinito
     
